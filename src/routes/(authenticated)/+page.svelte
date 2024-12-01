892bc60a316e71db9691ca2cc6d5492757d3d163
Mikel <61402923+Cypheriel@users.noreply.github.com>
2024-12-01 00:22:37 -0700

    update mockup card titles

--- src/routes/(authenticated)/+page.svelte
+++ src/routes/(authenticated)/+page.svelte
@@ -1,3 +1,4 @@
+
 <div class="flex items-center justify-center my-4">
 	<h1 class="text-4xl font-bold">Popular Shares</h1>
 </div>
@@ -6,9 +7,9 @@
 		<h2 class="text-2xl font-bold text-secondary mt-4 mb-4">Lith</h2>
 		<div class="carousel gap-x-4 overflow-auto w-full overflow-x-scroll">
 			{#each { length: 5 } as _, i}
-				<div class="carousel-item card bg-base-300 w-64 shadow-xl">
+				<div class="carousel-item card bg-base-300 w-72 shadow-xl">
 					<div class="card-body">
-						<h2 class="card-title">Lith Z1 (Intact)</h2>
+						<h2 class="card-title">Lith Z11 (Intact)</h2>
 						<div class="avatar-group -space-x-6 rtl:space-x-reverse">
 							<div class="avatar placeholder">
 								<div class="bg-orange-950 text-neutral-content w-12 rounded-full">
@@ -52,7 +53,7 @@
 		<h2 class="text-2xl font-bold text-secondary mt-8 mb-4">Meso</h2>
 		<div class="carousel gap-x-4 overflow-auto w-full overflow-x-scroll">
 			{#each { length: 5 } as _, i}
-				<div class="carousel-item card bg-base-300 w-64 shadow-xl">
+				<div class="carousel-item card bg-base-300 w-72 shadow-xl">
 					<div class="card-body">
 						<h2 class="card-title">Meso A9 (Radiant)</h2>
 						<div class="avatar-group -space-x-6 rtl:space-x-reverse">
@@ -88,9 +89,9 @@
 		<h2 class="text-2xl font-bold text-secondary mt-8 mb-4">Neo</h2>
 		<div class="carousel gap-x-4 overflow-auto w-full overflow-x-scroll">
 			{#each { length: 5 } as _, i}
-				<div class="carousel-item card bg-base-300 w-64 shadow-xl">
+				<div class="carousel-item card bg-base-300 w-72 shadow-xl">
 					<div class="card-body">
-						<h2 class="card-title">Meso A9 (Radiant)</h2>
+						<h2 class="card-title">Neo S13 (Radiant)</h2>
 						<div class="avatar-group -space-x-6 rtl:space-x-reverse">
 							<div class="avatar placeholder">
 								<div class="bg-gray-500 text-neutral-content w-12 rounded-full">
@@ -124,9 +125,9 @@
 		<h2 class="text-2xl font-bold text-secondary mt-8 mb-4">Axi</h2>
 		<div class="carousel gap-x-4 overflow-auto w-full overflow-x-scroll">
 			{#each { length: 5 } as _, i}
-				<div class="carousel-item card bg-base-300 w-64 shadow-xl">
+				<div class="carousel-item card bg-base-300 w-72 shadow-xl">
 					<div class="card-body">
-						<h2 class="card-title">Meso A9 (Radiant)</h2>
+						<h2 class="card-title">Axi V3 (Radiant)</h2>
 						<div class="avatar-group -space-x-6 rtl:space-x-reverse">
 							<div class="avatar placeholder">
 								<div class="bg-gray-500 text-neutral-content w-12 rounded-full">
@@ -160,9 +161,9 @@
 		<h2 class="text-2xl font-bold text-secondary mt-8 mb-4">Requiem</h2>
 		<div class="carousel gap-x-4 overflow-auto w-full overflow-x-scroll">
 			{#each { length: 5 } as _, i}
-				<div class="carousel-item card bg-base-300 w-64 shadow-xl">
+				<div class="carousel-item card bg-base-300 w-72 shadow-xl">
 					<div class="card-body">
-						<h2 class="card-title">Meso A9 (Radiant)</h2>
+						<h2 class="card-title">Requiem IV (Radiant)</h2>
 						<div class="avatar-group -space-x-6 rtl:space-x-reverse">
 							<div class="avatar placeholder">
 								<div class="bg-gray-500 text-neutral-content w-12 rounded-full">
@@ -172,11 +173,6 @@
 							<div class="avatar placeholder">
 								<div class="bg-gray-500 text-neutral-content w-12 rounded-full">
 									<span class="text-xs">U</span>
-								</div>
-							</div>
-							<div class="avatar placeholder">
-								<div class="bg-yellow-500 text-neutral-content w-12 rounded-full">
-									<span class="text-xs text-black">R</span>
 								</div>
 							</div>
 						</div>
